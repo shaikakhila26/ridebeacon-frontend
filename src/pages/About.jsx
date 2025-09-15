69358e56a4aaecf16e0db05942daa7c1a5566fe6
shaikakhila26 <akhilashaik2605@gmail.com>
2025-09-15 10:24:17 +0530

    about.jsx 3

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // You can update the className below with your actual background/theme classes.
 export default function About() {
